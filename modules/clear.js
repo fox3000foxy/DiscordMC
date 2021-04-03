09cc1f18b6124d758015846b67321d28bdc56fc2
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2021-04-03 16:53:51 +0200

    Add files via upload

--- modules/clear.js
+++ modules/clear.js
@@ -1,9 +1,15 @@
-function Info(message,channel)
-{
-	channel.send("You can have a view of your bot to http://localhost:"+message.author.discriminator);
+function Info(message, channel) {
+    let info = "```\n";
+    // info += "!panel for create a panel\n"
+    info += "!select[item_name] for select item\n"
+    info += "!craft[place][item_name] for set an item in case in your crafting table\n"
+    info += "!craft patern[item_name] for get craft pattern of[item_name]\n"
+    info += "```"
+    info += "You can have a view of your bot to http://localhost:" + message.author.discriminator
+    channel.send(info);
 }
 
-function clear(message){
+function clear(message) {
     message.delete()
     return message.channel.bulkDelete(100)
 }
--- modules/refreshStatus.js
+++ modules/refreshStatus.js
@@ -1,12 +1,20 @@
 let numberBots = require("./constants.js").numberBots
-
+let setStatus;
 function refreshStatus(client, plusorminus) {
+	clearInterval(setStatus)
 	if (plusorminus == "plus") numberBots++
 	else if (plusorminus == "minus") numberBots--
-	client.user.setActivity(numberBots + " bots", {
-		type: "STREAMING",
-		url: "https://twitch.tv/fox3000"
-	})
+	setStatus = setInterval(() => {
+		client.user.setActivity(numberBots + " bots", {
+			type: "STREAMING",
+			url: "https://twitch.tv/fox3000"
+		})
+		setTimeout(() => {
+			client.user.setActivity("!panel", {
+				type: "LISTENING",
+			})
+		}, 2500)
+	},5000)
 }
 
 exports.refreshStatus = refreshStatus
